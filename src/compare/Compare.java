5ac16659c0cd6f09f6c53556edb39b2785e0e379
Maciej <maciek.golianek@gmail.com>
2019-02-26 10:42:31 +0100

    add equals String

---
+++ src/compare/Compare.java
@@ -0,0 +1,13 @@
+package compare;
+
+public class Compare {
+    public static void main(String[] args) {
+        String imie = new String("Arek");
+        String imie2 = "Arek";
+
+        if (imie.equals(imie2))
+            System.out.println("Są  równe");
+        else System.out.println("nie są równe");
+
+    }
+}
